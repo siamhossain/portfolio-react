25999e6c73b6dc5f6b71e73db4f4504235a0375a
Siam Hossain <siam.hossain1994@gmail.com>
2022-05-26 02:52:55 +0600

    Updated global scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import './App.css';
 import './Static/StyleSheets/header.css';
-import Header from "./Components/Header/Header";
-import HeroSection from "./Components/HeroSection/HeroSection";
-import AboutSection from "./Components/AboutSection/AboutSection";
-import SkillsSection from "./Components/SkillsSection/SkillsSection";
-import EducationSection from "./Components/EducationSection/EducationSection";
-import ExperienceSection from "./Components/ExperienceSection/ExperienceSection";
-import ServicesSection from "./Components/ServicesSection/ServicesSection";
-import PortfolioSection from "./Components/PortfolioSection/PortfolioSection";
-import TestimonialSection from "./Components/TestimonialSection/TestimonialSection";
-import BlogSection from "./Components/BlogSection/BlogSection";
-import FooterSection from "./Components/FooterSection/FooterSection";
 import Home from "./Pages/Home/Home";
 import BlogList from "./Pages/BlogList/BlogList";
 import BlogDetails from "./Pages/BlogDetails/BlogDetails";
